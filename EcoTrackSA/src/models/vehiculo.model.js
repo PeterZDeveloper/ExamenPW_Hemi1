8c49d99e72fe94a9b9e3fa5ae5f798cfa39bf3f8
Jake24012001 <oberinho2401@outlook.com>
2025-10-20 08:39:46 -0500

    subdia

--- EcoTrackSA/src/models/vehiculo.model.js
+++ EcoTrackSA/src/models/vehiculo.model.js
@@ -1,34 +1,7 @@
 const db = require('../config/db');
 
-module.exports = (sequelize, DataTypes) => {
-  return sequelize.define('Vehiculo', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false
-    },
-    nombre: {
-      type: DataTypes.STRING(100),
-      allowNull: false
-    },
-    marca: {
-      type: DataTypes.STRING(50),
-      allowNull: false
-    },
-    anio: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    estado: {
-      type: DataTypes.STRING(20),
-      allowNull: true,
-      validate: {
-        isIn: [['activo', 'inactivo']] // Ajusta según tu restricción CHECK
-      }
-    }
-  }, {
-    tableName: 'vehiculos',
-    timestamps: false
-  });
-};
+exports.findAll= async()=>{
+  const result = await db.query("SELECT * FROM vehiculos");
+  return result.rows;
+}
+
