1cce3a3ea6a74ea32693080b4e624aed71ec7f3e
Jean Zambrano <149613108+jeanzambr@users.noreply.github.com>
2025-10-20 08:27:59 -0500

    creacion de componentes
    creacion de componentes vehiculos, models pages services

---
+++ EcoTrackSA-Frontend/ecotrack/src/app/vehiculos/pages/vehiculos-page/vehiculos-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehiculos-page',
+  imports: [],
+  templateUrl: './vehiculos-page.html',
+  styleUrl: './vehiculos-page.css'
+})
+export class VehiculosPage {
+
+}
